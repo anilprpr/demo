c79f9b256e894c1634b262ef0120dd55e618a145
anil <patelanil012@gmail.com>
2025-08-24 20:09:42 +0530

    added jenkins-demo project to local repo

---
+++ src/main/java/com/scale/demo/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.scale.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	
+		@PostMapping("/hello")
+		public String hello(@RequestParam  String name)
+		{
+			return name;
+			
+		}
+			
+
+}
